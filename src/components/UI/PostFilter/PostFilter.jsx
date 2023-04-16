5a5a75e8bde20bdd5311109ae6693b2a94d287f9
xkochevnikx <xkochevnik@protonmail.com>
2023-04-16 19:34:56 +0300

    fix

--- src/components/UI/PostFilter/PostFilter.jsx
+++ src/components/UI/PostFilter/PostFilter.jsx
@@ -1,29 +1,31 @@
-import React from "react";
-import MyInput from "../Input/MyInput";
-import MySelect from "../Select/MySelect";
+import React from 'react';
+import MyInput from '../Input/MyInput';
+import MySelect from '../Select/MySelect';
 
 const PostFilter = ({ filter, setFilter }) => {
-  //? в селекте onChange возвращает ни event а сразу выбранный критерий сортировки который мы при изменении записываем в стейт filter.sort
-  return (
-    <>
-      <MyInput
-        placeholder="поиск"
-        value={filter.searchQuery}
-        onChange={e => setFilter({ ...filter, searchQuery: e.target.value })}
-      />
-      <MySelect
-        value={filter.selectedSort}
-        onChange={selectedSort =>
-          setFilter({ ...filter, selectedSort: selectedSort })
-        }
-        defaultValue="Сортировка по"
-        option={[
-          { value: "title", name: "По названию" },
-          { value: "body", name: "По описанию" },
-        ]}
-      />
-    </>
-  );
+    //? селекте onChange возвращает ни event а сразу выбранный критерий сортировки который мы при изменении записываем в стейт filter.sort
+    return (
+        <>
+            <MyInput
+                placeholder='поиск'
+                value={filter.searchQuery}
+                onChange={(e) =>
+                    setFilter({ ...filter, searchQuery: e.target.value })
+                }
+            />
+            <MySelect
+                value={filter.selectedSort}
+                onChange={(selectedSort) =>
+                    setFilter({ ...filter, selectedSort: selectedSort })
+                }
+                defaultValue='Сортировка по'
+                option={[
+                    { value: 'title', name: 'По названию' },
+                    { value: 'body', name: 'По описанию' },
+                ]}
+            />
+        </>
+    );
 };
 
 export default PostFilter;
